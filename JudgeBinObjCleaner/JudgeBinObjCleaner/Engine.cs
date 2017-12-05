97c608e701367d7a9c5bbc92bc8f188e6c95d868
Radoslav Astardzhiev <freeccqeb@gmail.com>
2017-12-05 18:48:17 +0200

    Add "JudgeBinObjCleaner/JudgeBinObjCleaner/"

---
+++ JudgeBinObjCleaner/JudgeBinObjCleaner/Engine.cs
@@ -0,0 +1,66 @@
+﻿namespace RecycleBinObj
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class Engine
+    {
+        private readonly string[] TargetFolders = new[] { "bin", "obj" };
+
+        public void Run()
+        {
+            var startDirectory = "../../Recycler";
+            this.RemoveFolders(startDirectory);
+        }
+
+        private void RemoveFolders(string startDirectory)
+        {
+            var dirs = Directory.GetDirectories(startDirectory);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                var nameIndex = dirs[i].LastIndexOf('\\');
+                if (nameIndex < 0)
+                {
+                    nameIndex = dirs[i].LastIndexOf('/');
+                }
+
+                if (nameIndex < 0)
+                {
+                    nameIndex = 0;
+                }
+
+                var folderName = dirs[i]
+                    .Substring(nameIndex)
+                    .Trim('/', '\\');
+
+                if (TargetFolders.Any(name => name.EndsWith(folderName, StringComparison.OrdinalIgnoreCase) ||
+                    name.EndsWith(folderName, StringComparison.OrdinalIgnoreCase)))
+                {
+                    this.RemoveDirectory(dirs[i]);
+                }
+                else
+                {
+                    this.RemoveFolders(dirs[i]);
+                }
+            }
+        }
+
+        public void RemoveDirectory(string dir)
+        {
+            var files = Directory.GetFiles(dir);
+            for (int i = 0; i < files.Length; i++)
+            {
+                File.Delete(files[i]);
+            }
+
+            var dirs = Directory.GetDirectories(dir);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                this.RemoveDirectory(dirs[i]);
+            }
+
+            Directory.Delete(dir);
+        }
+    }
+}
---
+++ JudgeBinObjCleaner/JudgeBinObjCleaner/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace RecycleBinObj
+{
+    using System;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            new Engine().Run();
+            Console.WriteLine("Judge Ready :)");
+        }
+    }
+}
