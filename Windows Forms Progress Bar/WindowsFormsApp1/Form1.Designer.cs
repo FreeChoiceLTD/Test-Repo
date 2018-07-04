5d4eac5a09a437e156da7a0b68ea131672b1991d
Radoslav Astardzhiev <freeccqeb@gmail.com>
2018-07-04 22:52:59 +0300

    Windows Forms - Progress Bar test

---
+++ Windows Forms Progress Bar/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnContinous = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnMarquee = new System.Windows.Forms.Button();
+            this.tbValueProgress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnContinous
+            // 
+            this.btnContinous.Location = new System.Drawing.Point(23, 187);
+            this.btnContinous.Name = "btnContinous";
+            this.btnContinous.Size = new System.Drawing.Size(75, 23);
+            this.btnContinous.TabIndex = 0;
+            this.btnContinous.Text = "Continous";
+            this.btnContinous.UseVisualStyleBackColor = true;
+            this.btnContinous.Click += new System.EventHandler(this.btnContinous_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 31);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // btnMarquee
+            // 
+            this.btnMarquee.Location = new System.Drawing.Point(153, 187);
+            this.btnMarquee.Name = "btnMarquee";
+            this.btnMarquee.Size = new System.Drawing.Size(75, 23);
+            this.btnMarquee.TabIndex = 2;
+            this.btnMarquee.Text = "Marquee";
+            this.btnMarquee.UseVisualStyleBackColor = true;
+            this.btnMarquee.Click += new System.EventHandler(this.btnMarquee_Click);
+            // 
+            // tbValueProgress
+            // 
+            this.tbValueProgress.Location = new System.Drawing.Point(12, 37);
+            this.tbValueProgress.Name = "tbValueProgress";
+            this.tbValueProgress.Size = new System.Drawing.Size(260, 20);
+            this.tbValueProgress.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tbValueProgress);
+            this.Controls.Add(this.btnMarquee);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnContinous);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnContinous;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnMarquee;
+        private System.Windows.Forms.TextBox tbValueProgress;
+    }
+}
+
---
+++ Windows Forms Progress Bar/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMarquee_Click(object sender, EventArgs e)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            progressBar1.Visible = true;
+
+            // Display the ProgressBar control.
+            progressBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            progressBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            progressBar1.Maximum = 50;
+            // Set the initial value of the ProgressBar.
+            progressBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            progressBar1.Step = 1;
+            // Set a style
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.Value = 1;
+
+            // Loop through all files to copy.
+            int secs = timer.Elapsed.Seconds;
+            while (secs < 5)
+            {
+                tbValueProgress.Text = $"{secs}";
+                // Refresh to visualize
+                tbValueProgress.Refresh();
+
+                // Perform the increment on the ProgressBar.
+                progressBar1.Value = progressBar1.Value == progressBar1.Maximum
+                    ? progressBar1.Minimum
+                    : progressBar1.Value + progressBar1.Step;
+
+                // Sleep value controls the animation speed
+                Thread.Sleep(35);
+                secs = timer.Elapsed.Seconds;
+            }
+
+            progressBar1.Value = 1;
+            progressBar1.Visible = false;
+            timer.Stop();
+        }
+
+        private void btnContinous_Click_1(object sender, EventArgs e)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            progressBar1.Visible = true;
+
+            // Display the ProgressBar control.
+            progressBar1.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            progressBar1.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            progressBar1.Maximum = 100;
+            // Set the initial value of the ProgressBar.
+            progressBar1.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            progressBar1.Step = 1;
+            // Set a style
+            progressBar1.Style = ProgressBarStyle.Continuous;
+
+            // Loop through all files to copy.
+            while (timer.Elapsed.Seconds < 7)
+            {
+                // Perform the increment on the ProgressBar.
+                progressBar1.PerformStep();
+            }
+
+            progressBar1.Visible = false;
+            timer.Stop();
+        }
+    }
+}
